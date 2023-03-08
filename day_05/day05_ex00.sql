a220b9bbc255d2e65f330df4fb693de23f8d0ee2
CrazyTeg <alsair@bk.ru>
2023-03-08 15:46:15 +0700

    day_05_05

--- day_05/day05_ex00.sql
+++ day_05/day05_ex00.sql
@@ -29,8 +29,32 @@ join pizzeria as pizzeria_ on menu_.pizzeria_id = pizzeria_.id;
 create index idx_person_name on person (upper(name));
 explain analyze select name from person where upper(name) is not null;
 
+-- 03 Please create a better multicolumn B-Tree index with the name idx_person_order_multi for the SQL statement below.
+--    SELECT person_id, menu_id,order_date
+--    FROM person_order
+--    WHERE person_id = 8 AND menu_id = 19;
+-- The EXPLAIN ANALYZE command should return  the next pattern. Please be attention on "Index Only Scan" scanning!
+-- ...  Index Only Scan using idx_person_order_multi on person_order ...  
+-- Please provide any SQL with proof (EXPLAIN ANALYZE) that index idx_person_order_multi is working.
 
+create index idx_person_order_multi on person_order (person_id, menu_id, order_date);
+EXPLAIN ANALYZE select person_id, menu_id,order_date from person_order
+where person_id = 8 and menu_id = 19;
 
+-- 04 Please create a unique BTree index with the name idx_menu_unique on the menu table for  pizzeria_id and pizza_name columns.
+-- Please write and provide any SQL with proof (EXPLAIN ANALYZE) that index idx_menu_unique is working.
+
+create unique index idx_menu_unique on menu (pizzeria_id, pizza_name);
+explain analyze select pizzeria_id, pizza_name from menu;
+
+-- 05 Please create a partial unique BTree index with the name idx_person_order_order_date on the person_order table for person_id and menu_id attributes with partial uniqueness for order_date column for date ‘2022-01-01’.
+-- The EXPLAIN ANALYZE command should return  the next pattern
+-- ... Index Only Scan using idx_person_order_order_date on person_order …
+
+create unique index idx_person_order_order_date on person_order (person_id, menu_id)
+where order_date = '2022-01-01';
+explain analyze select person_id, menu_id from person_order
+where order_date = '2022-01-01';
 
 
 
