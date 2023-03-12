69b433b1f4ff8326afae2693702fd1f6196559e7
CrazyTeg <alsair@bk.ru>
2023-03-12 18:27:47 +0700

    day 06 04

--- day_06/day06_ex00.sql
+++ day_06/day06_ex00.sql
@@ -67,6 +67,20 @@ set enable_seqscan = off;
 create unique index idx_person_discounts_unique on person_discounts (person_id, pizzeria_id);
 explain analyze select person_id, pizzeria_id from person_discounts
 
+-- 04 Please add the following constraint rules for existing columns of the person_discounts table.
+
+-- . person_id column should not be NULL (use constraint name ch_nn_person_id)
+-- . pizzeria_id column should not be NULL (use constraint name ch_nn_pizzeria_id)
+-- . discount column should not be NULL (use constraint name ch_nn_discount)
+-- . discount column should be 0 percent by default
+-- . discount column should be in a range values from 0 to 100 (use constraint name ch_range_discount)
+
+alter table person_discounts
+add constraint ch_nn_person_id check (person_id is not null),
+add constraint ch_nn_pizzeria_id check (pizzeria_id is not null),
+add constraint ch_nn_discount check (discount is not null),
+alter discount set default 0, add constraint ch_range_discount check (discount between 0 and 100);
+
 
 
 
